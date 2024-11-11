e62ad6b319dd30926ea1b819b2e1f08bf233d182
Smart Me <146560332+1smartme@users.noreply.github.com>
2024-11-11 12:25:32 +0530

    Update 4-execv.c

--- 4-execv.c
+++ 4-execv.c
@@ -2,61 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-// ex1.c and bs1.c both are needed
-int bubleSort(int* arr,int len) {
+
+void bubbleSort(int* arr, int len) {
 	int temp;
-	for(int i=0; i<len; i++) {
-		for(int j=0; j<len-i-1; j++) {
-			if(arr[j+1] < arr[j]) {
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len - i - 1; j++) {
+			if (arr[j + 1] < arr[j]) {
 				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
 	}
-	printf("Sorted array:");
-	for(int i=0; i<len; i++) {
-		printf("%d \t",arr[i]);
-	}
-}
-int binarySearch(int* arr, int start, int end, int num) {
-	if(end >= start) {
-		int mid = start + (end - start)/2;
-		if(arr[mid] == num) return mid;
-		if(arr[mid] > num) return binarySearch(arr, start, mid-1, num);
-		if(arr[mid] < num) return binarySearch(arr, mid+1, end, num);
-	} else {
-		return -1;
-	}
-}
-int main(int argc, char** argv) {
-	int len = argc-1;
-	int arr[20];
-	int num;
-	printf("\nLength is %d\n", len-1);
-
-	for(int i=1; i<argc; i++) {
-		arr[i-1] = atoi(argv[i]);
-	}
-	bubleSort(arr,len);
-
-	printf("Reversed array:");
-	for(int i=len; i>=0; i--) {
+	printf("Sorted array: ");
+	for (int i = 0; i < len; i++) {
 		printf("%d \t", arr[i]);
 	}
+	printf("\n");
+}
 
-	printf("\nEnter Number to Search: ");
-	scanf("%d", &num);
-	int val = binarySearch(arr, 0, len-1, num);
-	if(val != -1) {
-		printf("\nThe Number is present\n");
-	} else {
-		printf("\nThe Number is Absent\n");
+int binarySearch(int* arr, int start, int end, int num) {
+	if (end >= start) {
+		int mid = start + (end - start) / 2;
+		if (arr[mid] == num) return mid;
+		if (arr[mid] > num) return binarySearch(arr, start, mid - 1, num);
+		return binarySearch(arr, mid + 1, end, num);
 	}
-	printf("BS process id: %d \n", getpid());
-	printf("BS parent process id: %d", getppid());
+	return -1;
+}
+
+int main(int argc, char** argv) {
+	int len = argc - 1;
+	int arr[20];
+	int num;
+	printf("Length is %d\n", len);
+
+	for (int i = 1; i < argc; i++) {
+		arr[i - 1] = atoi(argv[i]);
+	}
+
+	bubbleSort(arr, len);
+
+	printf("Reversed array: ");
+	for (int i = len - 1; i >= 0; i--) {
+		printf("%d \t", arr[i]);
+	}
+	printf("\n");
+
+	printf("Enter Number to Search: ");
+	scanf("%d", &num);
+
+	int val = binarySearch(arr, 0, len - 1, num);
+	if (val != -1) {
+		printf("The Number is present\n");
+	} else {
+		printf("The Number is Absent\n");
+	}
+
+	printf("BS process id: %d\n", getpid());
+	printf("BS parent process id: %d\n", getppid());
+
 	return 0;
 }
+
 
 
 
@@ -68,24 +76,44 @@ int main(int argc, char** argv) {
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-// ex1.c and bs1.c both are needed
+
 int main(int argc, char* argv[]) {
-	int status,i;
-	int count;
-	count = argc;
-	char* temp[count];
-	for(i=0; i<count; i++) {
-		temp[i] = argv[i+1];
-	}
-	temp[i] = NULL;
-	pid_t pid;
-	pid = fork();
-	if(pid == 0) {
-		execv(temp[0],temp);
-		printf("Helii");
-	} else {
-		wait(&status);
-		printf("\n Parent process id = %d", getpid());
+	int status, i;
+
+	// Ensure that the number of arguments is valid
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <program_to_execute> [args...]\n", argv[0]);
+		return 1;
 	}
 
+	// Create an array to hold command-line arguments for execv
+	char* temp[argc];
+	for (i = 0; i < argc - 1; i++) {
+		temp[i] = argv[i + 1]; // Copy arguments from argv (skipping the program name)
+	}
+	temp[argc - 1] = NULL; // Null-terminate the argument list for execv
+
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("Fork failed");
+		return 1;
+	}
+
+	if (pid == 0) { // Child process
+		// Execute the target program
+		if (execv(temp[0], temp) == -1) {
+			perror("execv failed"); // Print error if execv fails
+			exit(1);
+		}
+	} else { // Parent process
+		// Wait for the child process to complete
+		wait(&status);
+		if (WIFEXITED(status)) {
+			printf("\nChild process exited with status = %d\n", WEXITSTATUS(status));
+		}
+		printf("Parent process id = %d\n", getpid());
+	}
+
+	return 0;
 }
+
